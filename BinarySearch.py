8f00b74c0ea22a15dfd30dbeae6798f465252611
fauzank339 <50906187+fauzank339@users.noreply.github.com>
2022-04-08 21:13:55 +0530

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -12,3 +12,9 @@ def binarySearch(arr, target):
         else:
             return mid
     return -1
+
+''' Binary Search is the best searching algorith we have. It is very similar to how we search in a dictionary
+    First we find the middle position, then we check wheather our object of interest is preent threre, if yes then
+    we consider that we have achieved our goal and return that position, else we search in lower half or upper half
+    depending on wheather the object is most likey to be found in above half or lower half. there is usually a pre defined condition for it,
+    We keep repeating this proces until we find what we are looking for or we dont.
