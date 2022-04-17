a9e9356e4ef4eda0066583d3dd13e6dc100e2265
Abdullah Kidwai <50906187+fauzank339@users.noreply.github.com>
2022-04-17 23:56:25 +0530

    Update Ceiling.py

--- Ceiling.py
+++ Ceiling.py
@@ -12,3 +12,7 @@ def ceiling(arr,target):
         else:
             return mid
     return start
+
+Algorithm= It is the same as binary Search except for return start at the end
+That is because at that moment when conditon fails start is greater that end and points
+to the next element.
